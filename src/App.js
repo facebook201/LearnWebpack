939e6e271d2af4e2590518bb51d32bbae9d177c4
shiyao <email@example.com>
2019-07-09 07:53:03 +0800

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from 'antd/es/button';
 
-export default class App extends React.Component {
+
+
+class App extends React.Component {
   render() {
     return (
-      <div className="app">
-        <Button type="primary">Button</Button>
-      </div>
+      
     );
   }
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import zhCN from 'antd/es/locale-provider/zh_CN';
 import 'antd/dist/antd.css'
 
-import App from './App.js';
+import Home from './views/Home';
+import About from './views/About';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/home">Home</Link>
+      </li>
+    </ul>
+  );
+}
+
+ReactDOM.render((
+  <Router>
+    <div>
+      <Header />
+      <Route path="/home" component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+    </div>
+  </Router>
+  ), document.getElementById('root'));
